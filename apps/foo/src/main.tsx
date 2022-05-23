7d28ba901ea65e1cbe9577fde085b77d55cd5c61
EscapeB <haselczl@gmail.com>
2022-05-23 14:42:57 +0800

    chore: example for pnpm multiple instance

---
+++ apps/foo/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SimpleText } from 'components';
+
+export default class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div>
+        <SimpleText />
+      </div>
+    );
+  }
+}
